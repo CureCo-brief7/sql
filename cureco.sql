ac90668fd0d2a6fd7f5dd8120d35048abc7b5383
Marouane Bouchettoy <uanemaro216@gmail.com>
2023-01-23 00:06:47 +0100

    update

--- cureco.sql
+++ cureco.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : dim. 22 jan. 2023 à 02:27
+-- Généré le : lun. 23 jan. 2023 à 00:05
 -- Version du serveur : 10.4.27-MariaDB
 -- Version de PHP : 8.2.0
 
@@ -38,6 +38,18 @@ CREATE TABLE `product` (
   `User_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
+--
+-- Déchargement des données de la table `product`
+--
+
+INSERT INTO `product` (`id_p`, `Name`, `Description`, `Price`, `Quantity`, `Image`, `date`, `User_id`) VALUES
+(1, 'med1', 'med1', 12, 1222, 'm1.jpg', '2023-01-22', 5),
+(2, 'med2', 'med2', 12, 1234, 'm2.jpg', '2023-01-22', 5),
+(3, 'med3', 'med3', 12, 123, 'm3.jpg', '2023-01-22', 5),
+(4, 'med4', 'med4', 134, 345, 'm4.jpg', '2023-01-22', 5),
+(5, 'med5', 'med5', 23, 1234, 'm5.jpg', '2023-01-22', 5),
+(6, 'med6', 'med6', 123, 23, 'm6.jpg', '2023-01-22', 5);
+
 -- --------------------------------------------------------
 
 --
@@ -57,7 +69,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_u`, `userName`, `Email`, `Password`, `Role`) VALUES
-(4, 'marouane', 'uanemaro216@gmail.com', '$2y$10$3YoFMmE2puyMqvw3rWdH0.uqURsU1.aTH0jCxAEhs6FbCzR3b6qQm', 1);
+(5, 'marouane', 'uanemaro216@gmail.com', '$2y$10$ou9.waz8Lzw1eZYQPy1xk.B.E7qvMtlegCb.AfSLNP8nkrcwNSbrC', 1);
 
 --
 -- Index pour les tables déchargées
@@ -84,13 +96,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_p` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_u` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_u` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Contraintes pour les tables déchargées
